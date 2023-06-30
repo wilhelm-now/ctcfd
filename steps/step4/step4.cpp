d025b85c9511731a705a23848b3fe1f34db81f01
WILHELM <wilhelm.now@gmail.com>
2023-06-30 10:57:52 +0200

    Step4 Boilerplate copied from step3

---
+++ steps/step4/step4.cpp
@@ -0,0 +1,69 @@
+#include "step4_params.h"
+
+#include <util/typelist.h>
+#include <util/number.h>
+
+#include <util/for_i.h>
+#include <util/valuelist.h>
+
+#include <iostream>
+
+
+template<typename values>
+struct compute_burgers;
+
+template<typename previous, typename current>
+struct compute_burgers<typelist<previous, typelist<current, null_t> > >
+{
+	// RHS boundary stops computation and enforces boundary condition
+	typedef typename TYPELIST_1(NUMBER_MAKE(1.0)) type;
+};
+
+template<typename previous, typename current, typename next>
+class compute_burgers<typelist<previous, typelist<current, next> > >
+{
+	// u[next_t, current_idx] = u[current_t, current_idx] + (NU*DT/(DX*DX))*(u[current_t, next_idx] - 2*u[current_t, current_idx] + u[current_t, previous_idx])
+	typedef typename NUMBER_MAKE(NUMBER_GET_TYPE(current) + (NU*DT/(DX*DX)) *
+		(NUMBER_GET_TYPE(next::head) - 2*NUMBER_GET_TYPE(current) + NUMBER_GET_TYPE(previous))
+	)
+		computed;
+public:
+	typedef typelist<computed, typename compute_burgers<typename typelist<current, next> >::type> type;
+};
+
+// burgers 
+template<unsigned timestep>
+struct burgers;
+
+// initial condition meta-function, same as in step-2
+template<unsigned idx>
+struct initial
+{
+	typedef NUMBER_MAKE((idx* DX >= 0.5 && idx * DX <= 1.0) ? 2.0 : 1.0) type;
+};
+
+// apply initial condition
+template<>
+struct burgers<0>
+{
+	typedef for_i<NX, initial>::type type;
+};
+
+template<unsigned timestep>
+struct burgers
+{
+	typedef typelist<
+		// LHS boundary ignored
+		typename burgers<timestep - 1>::type::head,
+		// compute the rest of the domain with numerical scheme
+		typename compute_burgers<typename burgers<timestep - 1>::type>::type
+	> type;
+};
+
+int main()
+{
+	std::cout << "U0, U1 = [" << value_printer<burgers<0>::type>() << 
+		"], [" << value_printer<burgers<NT>::type>() << 
+		"]\n";
+}
+
