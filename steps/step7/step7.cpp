29c96f07e23af5e7c0988c955d5085f0243f8944
WILHELM <wilhelm.now@gmail.com>
2023-07-28 07:30:26 +0200

    Step7 Believe this is correct compute_diffusion_point function. Doesn't compile do to usual problem of ending recursion at the right time.

--- steps/step7/step7.cpp
+++ steps/step7/step7.cpp
@@ -2,7 +2,6 @@
 
 #include <util/typelist.h>
 #include <util/number.h>
-#include <util/point2d.h>
 
 #include <util/for_i.h>
 #include <util/conditional.h>
@@ -19,63 +18,59 @@
 struct initial
 {
 	template<unsigned index_i, unsigned index_j>
-	class func
+	struct func
 	{
 		typedef NUMBER_MAKE(
 			(0.5 <= DX * index_i && DX * index_i <= 1.0) && (0.5 <= DY * index_j && DY * index_j <= 1.0)
 			? 2.0 : 1.0)
-			number;
-	public:
-		typedef point2d_c<number, number> type;
+			type;
 	};
 };
 
 // for just a point
-template<
-	typename previous_x, // u[t, i-1, j]
-	typename previous_y, // u[t, i,   j-1]
-	typename current     // u[t, i,   j]
->
+template<TYPENAMES_5(px, nx, current, py, ny)> // previous and next x, current, previous and next y
 struct compute_diffusion_point
 {
-	// u[t+1, i, j] = u[t, i, j] - u[t, i, j]*(DT/DX)*(u[t, i, j] - u[t, i-1, j]) - v[t, i, j]*(DT/DY)*(u[t, i, j] - u[t, i, j-1])
-	// v[t+1, i, j] = v[t, i, j] - u[t, i, j]*(DT/DX)*(v[t, i, j] - v[t, i-1, j]) - v[t, i, j]*(DT/DY)*(v[t, i, j] - v[t, i, j-1])
-	typedef typename POINT_MAKE(
-		POINT_GET_U(current)
-		- POINT_GET_U(current) * (DT / DX) * (POINT_GET_U(current) - POINT_GET_U(previous_x))
-		- POINT_GET_V(current) * (DT / DY) * (POINT_GET_U(current) - POINT_GET_U(previous_y)),
-		POINT_GET_V(current)
-		- POINT_GET_U(current) * (DT / DX) * (POINT_GET_V(current) - POINT_GET_V(previous_x))
-		- POINT_GET_V(current) * (DT / DY) * (POINT_GET_V(current) - POINT_GET_V(previous_y))
+	// u[t+1, i, j] = u[t, i, j] 
+	//               + (NU*DT/(DX^2))*(u[t, i+1, j] - 2*u[t, i, j] + u[t, i-1, j])
+	//               + (NU*DT/(DY^2))*(u[t, i, j+1] - 2*u[t, i, j] + u[t, i, j-1])
+	typedef typename NUMBER_MAKE(
+		NUMBER_GET_TYPE(current)
+		+ (NU*DT/(DX*DX))*(NUMBER_GET_TYPE(nx) - 2*NUMBER_GET_TYPE(current) + NUMBER_GET_TYPE(px))
+		+ (NU*DT/(DY*DY))*(NUMBER_GET_TYPE(ny) - 2*NUMBER_GET_TYPE(current) + NUMBER_GET_TYPE(py))
 	) type;
 };
 
 // for just rows
-template<typename previous_row, typename current_row>
+template<typename previous_x_row, typename current_x_row, typename next_x_row>
 struct compute_diffusion_row;
 
-template<typename previous_x_values, typename previous_y>
-struct compute_diffusion_row<previous_x_values, typelist<previous_y, null_t> >
+template<typename previous_x_values, typename current_x_previous_y, typename current_x, typename next_x_row>
+struct compute_diffusion_row<
+	previous_x_values, 
+	typelist<current_x_previous_y, typelist<current_x, null_t> >,
+	next_x_row>
 {
 	typedef null_t type; // end of domain at y limit
 };
 
 template<
-	typename ignored_previous, // u[t, i-1, j-1]
-	typename previous_x,       // u[t, i-1, j]
-	typename next_previous_x,  // u[t, i-1, j + 1]
-	typename previous_y,       // u[t,   i, j-1]
-	typename current,          // u[t,   i, j]
-	typename next              // u[t,   i, j + 1]
+	TYPENAMES_3(pxpy, pxcy, pxny_tail), // u[t, i-1, j-1], u[t, i-1, j], u[t, i-1, j+1] with tail
+	TYPENAMES_3(cxpy, cxcy, cxny_tail), // u[t, i, j-1], u[t, i, j], u[t, i, j+1] with tail 
+	TYPENAMES_3(nxpy, nxcy, nxny_tail)  // u[t, i+1, j-1], u[[t, i+1, j], u[t, i+1, j+1] with tail
 >
 struct compute_diffusion_row<
-	typelist<ignored_previous, typelist<previous_x, next_previous_x> >,
-	typelist<previous_y, typelist<current, next> > >
+	TAILED_TYPELIST_2(pxpy, pxcy, pxny_tail),
+	TAILED_TYPELIST_2(cxpy, cxcy, cxny_tail),
+	TAILED_TYPELIST_2(nxpy, nxcy, nxny_tail)
+>
 {
-	typedef typelist<typename compute_diffusion_point<previous_x, previous_y, current>::type,
+	typedef typelist<
+		typename compute_diffusion_point<pxcy, nxcy, cxcy, cxpy, cxny_tail::head>::type,
 		typename compute_diffusion_row<
-		typelist<previous_x, next_previous_x>,
-		typelist<current, next> >::type
+			typelist<pxcy, pxny_tail>,
+			typelist<cxcy, cxny_tail>,
+			typelist<nxcy, nxny_tail> >::type
 	> type;
 };
 
@@ -83,21 +78,21 @@ struct compute_diffusion_row<
 template<typename values>
 struct compute_diffusion;
 
-template<typename previous_x>
-struct compute_diffusion<typelist<previous_x, null_t> >
+template<typename previous_x, typename current_x>
+struct compute_diffusion<typelist<previous_x, typelist<current_x, null_t> > >
 {
 	typedef null_t type; // end of domain at x limit
 };
 
-template<typename previous_x, typename current_x, typename next_x>
-struct compute_diffusion<typelist<previous_x, typelist<current_x, next_x> > >
+template<typename previous_x, typename current_x, typename next_x, typename x_tail>
+struct compute_diffusion<TAILED_TYPELIST_3(previous_x, current_x, next_x, x_tail)>
 {
 	typedef typelist<
-		typelist<POINT_MAKE(1, 1), // boundary condition and rest of calculatin
-		typename compute_diffusion_row<previous_x, current_x>::type
+		typelist<NUMBER_MAKE(1), // boundary condition and rest of calculatin
+		typename compute_diffusion_row<previous_x, current_x, next_x>::type
 		>
 		,
-		typename compute_diffusion<typelist<current_x, next_x> >::type
+		typename compute_diffusion<typelist<current_x, typelist<next_x, x_tail> > >::type
 	> type;
 };
 
@@ -110,7 +105,6 @@ struct diffusion2d<0> // save initial conditions
 	typedef for_ij<NX, NY, initial>::type type;
 };
 
-// Boundary conditions is u, v = 1 at each edge
 // X boundary saved here
 typedef diffusion2d<0>::type::head x_boundary;
 
