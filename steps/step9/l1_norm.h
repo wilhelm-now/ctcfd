a1e46670b863ec0550dcf8b57570ae037a2f07c0
WILHELM <wilhelm.now@gmail.com>
2023-08-19 14:44:09 +0200

    Step9 L1 norm is fast tracked but the convergence function is wrong.

--- steps/step9/l1_norm.h
+++ steps/step9/l1_norm.h
@@ -9,23 +9,41 @@
 template<typename values>
 struct l1_norm;
 
+// base case
 template<>
 struct l1_norm<null_t>
 {
 	enum {value = 0};
 };
 
+// iteration over a row
 template<long long num, typename tail>
 struct l1_norm<typelist<number_c<num>, tail> >
 {
 	enum { value = CTCFD_ABS(num) + l1_norm<tail>::value };
 };
 
-template<long long num, typename tail>
-struct l1_norm<typelist<number_c<num>, tail> >
+// iteration over rows
+template<typename current_row, typename next_rows>
+struct l1_norm<typelist<current_row, next_rows> >
 {
-	enum { value = CTCFD_ABS(num) + l1_norm<tail>::value };
+	enum { value = l1_norm<current_row>::value + l1_norm<next_rows>::value };
 };
+
+
+template<typename values_current, typename values_prev>
+struct l1_norm_convergence : NUMBER_MAKE(CTCFD_ABS((NUMBER_GET_RAW(l1_norm<values_current>::value) - NUMBER_GET_RAW(l1_norm<values_prev>::value)) / NUMBER_GET_RAW(l1_norm<values_prev>::value)))
+{
+};
+
+// Fast tracks for a single row below
+
+template<long long x1, long long x2, long long x3, long long x4, typename tail>
+struct l1_norm<TAILED_TYPELIST_4(number_c<x1>, number_c<x2>, number_c<x3>, number_c<x4>, tail)>
+{
+	enum { value = CTCFD_ABS(x1) + CTCFD_ABS(x2) + CTCFD_ABS(x3) + CTCFD_ABS(x4) + l1_norm<tail>::value };
+};
+
 
 template<long long x1, long long x2, long long x3, long long x4, long long x5, long long x6, long long x7, long long x8, typename tail>
 struct l1_norm<TAILED_TYPELIST_8(number_c<x1>, number_c<x2>, number_c<x3>, number_c<x4>, number_c<x5>, number_c<x6>, number_c<x7>, number_c<x8>, tail)>
@@ -34,15 +52,25 @@ struct l1_norm<TAILED_TYPELIST_8(number_c<x1>, number_c<x2>, number_c<x3>, numbe
 };
 
 
-template<typename current_row, typename next_rows>
-struct l1_norm<typelist<current_row, next_rows> >
+template<long long x1, long long x2, long long x3, long long x4, long long x5, long long x6, long long x7, long long x8,
+	long long x9, long long x10, long long x11, long long x12, long long x13, long long x14, long long x15, long long x16, typename tail>
+struct l1_norm<TAILED_TYPELIST_16(number_c<x1>, number_c<x2>, number_c<x3>, number_c<x4>, number_c<x5>, number_c<x6>, number_c<x7>, number_c<x8>, number_c<x9>, number_c<x10>, number_c<x11>, number_c<x12>, number_c<x13>, number_c<x14>, number_c<x15>, number_c<x16>, tail)>
 {
-	enum { value = l1_norm<current_row>::value + l1_norm<next_rows>::value };
+	enum { value = 
+		CTCFD_ABS(x1) + CTCFD_ABS(x2) + CTCFD_ABS(x3) + CTCFD_ABS(x4) + CTCFD_ABS(x5) + CTCFD_ABS(x6) + CTCFD_ABS(x7) + 
+		CTCFD_ABS(x8) + CTCFD_ABS(x9) + CTCFD_ABS(x10) + CTCFD_ABS(x11) + CTCFD_ABS(x12) + CTCFD_ABS(x13) + CTCFD_ABS(x14) 
+		+ CTCFD_ABS(x15) + CTCFD_ABS(x16) + l1_norm<tail>::value };
 };
 
-template<typename values_current, typename values_prev>
-struct l1_norm_convergence : NUMBER_MAKE((NUMBER_GET_RAW(l1_norm<values_current>::value) - NUMBER_GET_RAW(l1_norm<values_prev>::value)) / NUMBER_GET_RAW(l1_norm<values_prev>::value))
+// Fast tracks for multiple rows
+
+template<TYPENAMES_9(T1A, T1B, T2A, T2B, T3A, T3B, T4A, T4B, tail)>
+struct l1_norm<TAILED_TYPELIST_4(TAILED_TYPELIST_1(T1A, T1B), TAILED_TYPELIST_1(T2A, T2B), TAILED_TYPELIST_1(T3A, T3B), TAILED_TYPELIST_1(T4A, T4B), tail)>
 {
+	enum {value = 
+		l1_norm<typelist<T1A, T1B> >::value + l1_norm<typelist<T2A, T2B> >::value
+		+ l1_norm<typelist<T3A, T3B> >::value + l1_norm<typelist<T4A, T4B> >::value
+		+ l1_norm<tail>::value};
 };
 
 #endif // ! L1_NORM_H
