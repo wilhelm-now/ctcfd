3e761e6d017680cfa36e1315fc25b5917230fe6d
WILHELM <wilhelm.now@gmail.com>
2023-09-15 20:05:51 +0200

    Step10 Fast-track jacobi solution.

--- steps/step10/step10.cpp
+++ steps/step10/step10.cpp
@@ -40,31 +40,12 @@ struct compute_poisson_point
 template<typename previous_x_row, typename current_x_row, typename next_x_row, typename current_b_row>
 class compute_poisson_row;
 
-// Specialization to apply boundary condition on far y-limit.
-template<
-	TYPENAMES_3(pxpy, pxcy, pxny), // p for previous, c for current, n for next. x for x, y for y.
-	TYPENAMES_3(cxpy, cxcy, cxny), // pxpy = p[i-1, j-1], cxny = p[i, j+1]
-    TYPENAMES_3(nxpy, nxcy, nxny),
-	TYPENAMES_3(cxpy_b, cxcy_b, cxny_b)> // b[i, j-1], b[i, j], b[i, j+1]
-class compute_poisson_row<
-	TYPELIST_3(pxpy, pxcy, pxny),
-	TYPELIST_3(cxpy, cxcy, cxny),
-	TYPELIST_3(nxpy, nxcy, nxny),
-	TYPELIST_3(cxpy_b, cxcy_b, cxny_b)>
+//Specialization to apply boundary condition on far y-limit.
+template<TYPENAMES_2(pxpy, pxcy), TYPENAMES_2(cxpy, cxcy), TYPENAMES_2(nxpy, nxcy), TYPENAMES_2(cxpy_b, cxcy_b)>
+class compute_poisson_row<TYPELIST_2(pxpy, pxcy), TYPELIST_2(cxpy, cxcy), TYPELIST_2(nxpy, nxcy), TYPELIST_2(cxpy_b, cxcy_b)>
 {
-  typedef typename compute_poisson_point<pxcy, nxcy, cxpy, cxny, cxcy_b>::type computed;
-#if defined(CTCFD_POISSON_SOR)
-	typedef NUMBER_MAKE((1 - CTCFD_POISSON_SOR_OMEGA) * NUMBER_GET_TYPE(computed) + CTCFD_POISSON_SOR_OMEGA * NUMBER_GET_TYPE(computed)) sor_computed;
-#endif
-
 public:
-	// Stops recursion
-	// And sets p=0 at boundary
-#if defined(CTCFD_POISSON_SOR)
-	typedef TYPELIST_2(sor_computed, NUMBER_MAKE(0.0)) type;
-#else
-	typedef TYPELIST_2(computed, NUMBER_MAKE(0.0)) type;
-#endif
+	typedef TYPELIST_1(NUMBER_MAKE(0.0)) type;
 };
 
 template<
@@ -96,6 +77,73 @@ public:
 		TAILED_TYPELIST_2(nxcy, nxny, nxtaily),
 		TAILED_TYPELIST_2(cxcy_b, cxny_b, cxtaily_b)>::type > type;
 };
+
+#ifdef CTCFD_FASTTRACKED
+#ifdef CTCFD_POISSON_JACOBI
+
+template<
+	TYPENAMES_7(px_y1, px_y2, px_y3, px_y4, px_y5, px_y6, px_ytail),
+	TYPENAMES_7(cx_y1, cx_y2, cx_y3, cx_y4, cx_y5, cx_y6, cx_ytail),
+	TYPENAMES_7(nx_y1, nx_y2, nx_y3, nx_y4, nx_y5, nx_y6, nx_ytail),
+	TYPENAMES_7(cx_b1, cx_b2, cx_b3, cx_b4, cx_b5, cx_b6, cx_btail)>
+class compute_poisson_row<
+	TAILED_TYPELIST_6(px_y1, px_y2, px_y3, px_y4, px_y5, px_y6, px_ytail),
+	TAILED_TYPELIST_6(cx_y1, cx_y2, cx_y3, cx_y4, cx_y5, cx_y6, cx_ytail),
+	TAILED_TYPELIST_6(nx_y1, nx_y2, nx_y3, nx_y4, nx_y5, nx_y6, nx_ytail),
+	TAILED_TYPELIST_6(cx_b1, cx_b2, cx_b3, cx_b4, cx_b5, cx_b6, cx_btail)>
+{
+public:
+	typedef typelist<typename compute_poisson_point<px_y2, nx_y2, cx_y1, cx_y3, cx_b2>::type,
+		typelist<typename compute_poisson_point<px_y3, nx_y3, cx_y2, cx_y4, cx_b3>::type,
+		typelist<typename compute_poisson_point<px_y4, nx_y4, cx_y3, cx_y5, cx_b4>::type,
+		typelist<typename compute_poisson_point<px_y5, nx_y5, cx_y4, cx_y6, cx_b5>::type,
+		typename compute_poisson_row<
+			TAILED_TYPELIST_2(px_y5, px_y6, px_ytail),
+			TAILED_TYPELIST_2(cx_y5, cx_y6, cx_ytail),
+			TAILED_TYPELIST_2(nx_y5, nx_y6, nx_ytail),
+			TAILED_TYPELIST_2(cx_b5, cx_b6, cx_btail)>::type
+		> > > > type;
+};
+
+
+template<
+	TYPENAMES_19(px_y1, px_y2, px_y3, px_y4, px_y5, px_y6, px_y7, px_y8, px_y9, px_y10, px_y11, px_y12, px_y13, px_y14, px_y15, px_y16, px_y17, px_y18, px_ytail),
+	TYPENAMES_19(cx_y1, cx_y2, cx_y3, cx_y4, cx_y5, cx_y6, cx_y7, cx_y8, cx_y9, cx_y10, cx_y11, cx_y12, cx_y13, cx_y14, cx_y15, cx_y16, cx_y17, cx_y18, cx_ytail),
+	TYPENAMES_19(nx_y1, nx_y2, nx_y3, nx_y4, nx_y5, nx_y6, nx_y7, nx_y8, nx_y9, nx_y10, nx_y11, nx_y12, nx_y13, nx_y14, nx_y15, nx_y16, nx_y17, nx_y18, nx_ytail),
+	TYPENAMES_19(cx_b1, cx_b2, cx_b3, cx_b4, cx_b5, cx_b6, cx_b7, cx_b8, cx_b9, cx_b10, cx_b11, cx_b12, cx_b13, cx_b14, cx_b15, cx_b16, cx_b17, cx_b18, cx_btail)>
+class compute_poisson_row<
+	TAILED_TYPELIST_18(px_y1, px_y2, px_y3, px_y4, px_y5, px_y6, px_y7, px_y8, px_y9, px_y10, px_y11, px_y12, px_y13, px_y14, px_y15, px_y16, px_y17, px_y18, px_ytail),
+	TAILED_TYPELIST_18(cx_y1, cx_y2, cx_y3, cx_y4, cx_y5, cx_y6, cx_y7, cx_y8, cx_y9, cx_y10, cx_y11, cx_y12, cx_y13, cx_y14, cx_y15, cx_y16, cx_y17, cx_y18, cx_ytail),
+	TAILED_TYPELIST_18(nx_y1, nx_y2, nx_y3, nx_y4, nx_y5, nx_y6, nx_y7, nx_y8, nx_y9, nx_y10, nx_y11, nx_y12, nx_y13, nx_y14, nx_y15, nx_y16, nx_y17, nx_y18, nx_ytail),
+	TAILED_TYPELIST_18(cx_b1, cx_b2, cx_b3, cx_b4, cx_b5, cx_b6, cx_b7, cx_b8, cx_b9, cx_b10, cx_b11, cx_b12, cx_b13, cx_b14, cx_b15, cx_b16, cx_b17, cx_b18, cx_btail)>
+{
+public:
+	typedef typelist<typename compute_poisson_point<px_y2, nx_y2, cx_y1, cx_y3, cx_b2>::type,
+		typelist<typename compute_poisson_point<px_y3, nx_y3, cx_y2, cx_y4, cx_b3>::type,
+		typelist<typename compute_poisson_point<px_y4, nx_y4, cx_y3, cx_y5, cx_b4>::type,
+		typelist<typename compute_poisson_point<px_y5, nx_y5, cx_y4, cx_y6, cx_b5>::type,
+		typelist<typename compute_poisson_point<px_y6, nx_y6, cx_y5, cx_y7, cx_b6>::type,
+		typelist<typename compute_poisson_point<px_y7, nx_y7, cx_y6, cx_y8, cx_b7>::type,
+		typelist<typename compute_poisson_point<px_y8, nx_y8, cx_y7, cx_y9, cx_b8>::type,
+		typelist<typename compute_poisson_point<px_y9, nx_y9, cx_y8, cx_y10, cx_b9>::type,
+		typelist<typename compute_poisson_point<px_y10, nx_y10, cx_y9, cx_y11, cx_b10>::type,
+		typelist<typename compute_poisson_point<px_y11, nx_y11, cx_y10, cx_y12, cx_b11>::type,
+		typelist<typename compute_poisson_point<px_y12, nx_y12, cx_y11, cx_y13, cx_b12>::type,
+		typelist<typename compute_poisson_point<px_y13, nx_y13, cx_y12, cx_y14, cx_b13>::type,
+		typelist<typename compute_poisson_point<px_y14, nx_y14, cx_y13, cx_y15, cx_b14>::type,
+		typelist<typename compute_poisson_point<px_y15, nx_y15, cx_y14, cx_y16, cx_b15>::type,
+		typelist<typename compute_poisson_point<px_y16, nx_y16, cx_y15, cx_y17, cx_b16>::type,
+		typelist<typename compute_poisson_point<px_y17, nx_y17, cx_y16, cx_y18, cx_b17>::type,
+		typename compute_poisson_row<
+			TAILED_TYPELIST_2(px_y17, px_y18, px_ytail),
+			TAILED_TYPELIST_2(cx_y17, cx_y18, cx_ytail),
+			TAILED_TYPELIST_2(nx_y17, nx_y18, nx_ytail),
+			TAILED_TYPELIST_2(cx_b17, cx_b18, cx_btail)>::type
+		> > > > > > > > > > > > > > > > type;
+};
+
+#endif // CTCFD_POISSON_JACOBI
+#endif // CTCFD_FASTTRACKED
 
 template<typename values, typename source_b>
 class compute_poisson;
